d9330fbbbedb4d4b939eca01ec1bf1fe7450e615
Gurkeeratt <71377453+Gurkeeratt@users.noreply.github.com>
2020-12-09 21:43:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var hour, minute, second;
+
+
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
+
+scAngle = map(sc,0,60,0,360);
+mnAngle = map(mn,0,60,0,360);
+hrAngle = map(hr%12,0,12,0,360);
+
+push();
+rotate(scAngle);
+stroke(255,0,0);
+strokeWeight(7);
+line(0,0,100,0);
+pop();
